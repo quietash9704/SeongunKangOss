fe5c54d2710d5a68ff912799c8d9d75c0ce4c6e5
JaeYong Cho <quietash9704@gmail.com>
2018-04-09 01:52:03 -0700

    pull request

--- helloWorld.c
+++ helloWorld.c
@@ -1,6 +1,6 @@
-#include <stdio.h>
+#include <stdio.h>	//standard input output header file
 int main(void)
 {
-	printf("hello World");
-	printf("hello World!");
+	printf("hello World");	//print hello World
+	printf("hello World!");	//print hello World!
 }
